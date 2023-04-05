b24e87941d6a742fd3ddc2ce3a5a7bbe10d4597a
Kirill <binom2002@gmail.com>
2023-04-05 13:11:03 +0300

    Added news from python.org, bootstrap

--- web.py
+++ web.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 
 from weather import weather_by_city
+from org_news import get_news
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = "Weather report"
+    title = "Weather/News report"
     weather = weather_by_city("Lviv,Ukraine")
-    return render_template('index.html', page_title=title, weather=weather)
+    news_list = get_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
 if __name__ == '__main__':
     app.run(debug=True)   
