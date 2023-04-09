a4d541560de1972f5fcae774d3cb1e61841ee6e7
Kirill <binom2002@gmail.com>
2023-04-09 14:34:11 +0300

    Added log in feature< added user in db, created login, admin indexes

--- webapp/config.py
+++ webapp/config.py
@@ -6,3 +6,4 @@ WEATHER_API_KEY = "73c921fc0aa94988af5105517230404"
 WEATHER_DEFAULT_CITY= "Lviv,Ukraine"
 WEATHER_URL ='http://api.worldweatheronline.com/premium/v1/weather.ashx' 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+SECRET_KEY = "ADAD%Aa5dsacd6^D&ajasdaksad"
