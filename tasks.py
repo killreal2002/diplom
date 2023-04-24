c3ceeaa66f6ce9b0f797ba0125475a1a09812c7f
Kirill <binom2002@gmail.com>
2023-04-24 15:27:33 +0300

    added automatic scheduler for news snippets

---
+++ tasks.py
@@ -0,0 +1,25 @@
+from celery import Celery
+from celery.schedules import crontab
+from redis import Redis
+
+from webapp import create_app
+from webapp.news.parsers import dou
+from webapp.org_news import get_news
+
+flask_app = create_app()
+celery_app = Celery('task', broker='redis://localhost:6379/0')
+
+@celery_app.task
+def dou_snippets():
+    with flask_app.app_context():
+        dou.get_news_snippets()
+
+@celery_app.task
+def dou_content():
+    with flask_app.app_context():
+        dou.get_news_content()
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(crontab(minute='*/1'), dou_snippets.s())
+    sender.add_periodic_task(crontab(minute='*/1'), dou_content.s())
